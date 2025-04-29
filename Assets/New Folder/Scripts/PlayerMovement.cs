497d3014abb4ee790cba2453a1a0f201a78122e2
Tatyana Chebotova <tanyachebotova230@gmail.com>
2025-04-29 13:38:21 +0300

    camera control

--- Assets/New Folder/Scripts/PlayerMovement.cs
+++ Assets/New Folder/Scripts/PlayerMovement.cs
@@ -1,36 +1,56 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float fallThreshold = -10f;
     private Rigidbody2D body;
+    private SpriteRenderer spriteRenderer;
     private BoxCollider2D boxCollider;
     private float horizontalInput;
+    private bool isFalling = false;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        // if (body.velocity.x > 0.1f)
+        // {
+        //     spriteRenderer.flipX = false;
+        // }
+        // else if (body.velocity.x < -0.1f)
+        // {
+        //     spriteRenderer.flipX = true;
+        // }
         horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
         if (horizontalInput > 0.01f)
-            transform.localScale = new Vector3((float)0.08, (float)0.08, (float)0.08);
+            spriteRenderer.flipX = false;
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3((float)-0.08, (float)0.08, (float)0.08);
+            spriteRenderer.flipX = true;
+      
 
         if (Input.GetButtonDown("Jump") && isGrounded())
             Jump();
     }
 
+    private void FixedUpdate()
+    {
+        CheckFall();
+    }
+
     private void Jump()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
+        body.velocity = new Vector2(body.velocity.x, speed + 4);
     }
 
     private bool isGrounded()
@@ -42,5 +62,23 @@ public class PlayerMovement : MonoBehaviour
     public bool canAttack()
     {
         return horizontalInput == 0;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void CheckFall()
+    {
+        if (transform.position.y < fallThreshold && !isFalling)
+        {
+            isFalling = true;
+            RestartLevel();
+        }
+        else if (transform.position.y < fallThreshold)
+        {
+            isFalling = false;
+        }
     }
 }
